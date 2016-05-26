38dba7e4b4ec99d07c0c3068f2a19cd4b25919a4
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-05-26 09:40:43 -0700

    allowed display label to show digit on first press and append using the "+" afterwards

--- stanford/assignment1-calculator/assignment1-calculator/ViewController.swift
+++ stanford/assignment1-calculator/assignment1-calculator/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBOutlet weak var display: UILabel!
+    @IBAction func tappedButton(sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurentlyInDisplay = display.text!
+            display.text = textCurentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+        
+//        var number: String? = nil
+//        if number == nil {
+//            number = sender.currentTitle!
+//            answerScreen.text = number!
+//        } else {
+//            number = sender.currentTitle!
+//            answerScreen.text?.stringByAppendingString(number!)
+//        }
+//        print("\(number) was pressed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
