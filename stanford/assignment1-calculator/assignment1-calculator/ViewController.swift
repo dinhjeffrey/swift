ca31185047803f5203050c0afbec19681aa5d795
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-05-30 11:47:19 -0700

    highlight and unhighlight feature on binary operators working! ^_^

--- stanford/assignment1-calculator/assignment1-calculator/ViewController.swift
+++ stanford/assignment1-calculator/assignment1-calculator/ViewController.swift
@@ -23,23 +23,20 @@ class ViewController: UIViewController {
     private var userPressedPeriod = false
     private var userPressedBinaryOperator = false
     private var brain = CalculatorBrain()
+    private var binaryOperatorsInArray = ["−", "+", "÷", "×", "E", "xʸ"]
+    private var binaryOperatorToUnhighlight = UIButton?()
     
-    /*
-     computed property (vs stored property var)
-     whenever we get the display, we get it returned as a Double
-     we can also set it
-     */
     private var displayValue: String {
         get {
             print("in displayValue get")
             if display.text != "." {
-                return display.text! // unwrapped because it might not be convertable (e.g. "hello")
+                return display.text!
             }
             return "0" // to capture for sqrt(.)
         }
         set {
             print("in displayValue set")
-            display.text = String(newValue) // newValue is a special keyword. use (e.g. displayValue = M_PI vs. display.text = String(M_PI)
+            display.text = String(newValue) // newValue is a special keyword.
         }
     }
     private var sequenceValue: String {
@@ -51,6 +48,14 @@ class ViewController: UIViewController {
             print("sequenceValue set")
             sequence.text = newValue
         }
+    }
+    func unhighlight(button: UIButton) {
+        button.layer.borderWidth = 0
+    }
+    func highlight(button : UIButton) {
+        let redBorder = UIColor( red: 0, green: 0, blue:0, alpha: 0.3 )
+        button.layer.borderWidth = 4
+        button.layer.borderColor = redBorder.CGColor
     }
     
     @IBAction private func tappedButton(sender: UIButton) {
@@ -72,7 +77,6 @@ class ViewController: UIViewController {
             }
         }
         userIsInTheMiddleOfTyping = true
-        
     }
     @IBAction func period(sender: UIButton) {
         print("in func period")
@@ -83,7 +87,7 @@ class ViewController: UIViewController {
             userPressedPeriod = true
             userIsInTheMiddleOfTyping = true
         }
-        if !userPressedPeriod { // userIsInTheMiddleOfTyping is already true by default or else it would hit the previous if block
+        if !userPressedPeriod {
             print("in func period !userPressedPeriod")
             displayValue += period
             userPressedPeriod = true
@@ -95,56 +99,24 @@ class ViewController: UIViewController {
         sequenceValue = "0"
         print("in func clearAll")
     }
-    
-    /*
-     the code to hook our model to controller
-     */
     @IBAction private func tappedOperation(sender: UIButton) {
-        //if userIsInTheMiddleOfTyping { // don't need because default value is 0. if uncommented, CalculatorBrain default accumulator is 0.0 already. When just setting math symbols, it will use default accumulator value of 0.0
         print("in func tappedOperation")
         userPressedPeriod = false
         userIsInTheMiddleOfTyping = false
         print("sending displayValue to brain.setOperand")
         brain.setOperand(Double(displayValue)!)
-        //}
         if let mathematicalSymbol = sender.currentTitle {
-            print("in mathematicalSymbol. mathematicalSymbol is \(mathematicalSymbol). userPressedBinaryOperator is \(userPressedBinaryOperator)")
-            //            guard !userPressedBinaryOperator else { return } // return if user did press binary op
-            //            switch mathematicalSymbol {
-            //            case "+": userPressedBinaryOperator = true
-            //                brain.performOperation(mathematicalSymbol)
-            //            case "-": userPressedBinaryOperator = true
-            //                brain.performOperation(mathematicalSymbol)
-            //            case "÷": userPressedBinaryOperator = true
-            //                brain.performOperation(mathematicalSymbol)
-            //            case "×": userPressedBinaryOperator = true
-            //                brain.performOperation(mathematicalSymbol)
-            //            default: brain.performOperation(mathematicalSymbol)
-            //            }
-//            if (mathematicalSymbol == "+" && !userPressedBinaryOperator) || (mathematicalSymbol == "−" && !userPressedBinaryOperator) || (mathematicalSymbol == "×" && !userPressedBinaryOperator) ||  (mathematicalSymbol == "÷" && !userPressedBinaryOperator) {
-//                print("in mathematicalSymbol binaryOp && !userPressedBinaryOperator")
-//                userPressedBinaryOperator = true
-//                print("sending '\(mathematicalSymbol)' to brain.performOperation")
-//                brain.performOperation(mathematicalSymbol)
-//            } else if userPressedBinaryOperator && mathematicalSymbol != "=" {
-//                print("in mathematicalSymbol binaryOp && mathematicalSymbol != =")
-//                return
-//            } else { // user pressed an unary operator or "="
-//                print("in mathematicalSymbol else")
-                brain.performOperation(mathematicalSymbol)
-//            }
-            /*
-             guard let title = button.currentitle, operation = operations[title] else {
-             return
-             }
-             if case .binary(let method) = operation {
-             print(method(3,2))
-             }
-             else if .unary(let method) = operation {
-             print(method(3))
-             }
-             */
-            print("mathematicalSymbol is \(mathematicalSymbol)")
+            // highlight
+            if (binaryOperatorToUnhighlight != nil) {
+                unhighlight(binaryOperatorToUnhighlight!)
+            }
+            if binaryOperatorsInArray.contains(mathematicalSymbol) {
+                highlight(sender)
+                binaryOperatorToUnhighlight = sender
+            }
+            // end:highlight
+            print("in mathematicalSymbol. mathematicalSymbol is \(mathematicalSymbol). userPressedBinaryOperator is \(userPressedBinaryOperator) and sending mathematicalSymbol to brain.performOperation")
+            brain.performOperation(mathematicalSymbol)
             sequenceValue += mathematicalSymbol
             print("brain.isPartialResult is \(brain.isPartialResult) and !brain.isPartialResult is \(!brain.isPartialResult)")
             if brain.isPartialResult {
@@ -162,3 +134,14 @@ class ViewController: UIViewController {
     }
 }
 
+/*
+ guard let title = button.currentitle, operation = operations[title] else {
+ return
+ }
+ if case .binary(let method) = operation {
+ print(method(3,2))
+ }
+ else if .unary(let method) = operation {
+ print(method(3))
+ }
+ */
