496811e9c750f182959b56d109f421519438cf39
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-06-09 16:03:26 -0700

    added stanford lecture 4

--- etc/stanford-lecture-4.playground/Contents.swift
+++ etc/stanford-lecture-4.playground/Contents.swift
@@ -2,4 +2,25 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Create a UIBezierPath
+let path = UIBezierPath()
+
+// Move around, add lines or arcs to the path
+path.moveToPoint(CGPoint(x: 80, y: 50))
+path.addLineToPoint(CGPoint(x: 140, y: 150))
+path.addLineToPoint(CGPoint(x: 10, y: 150))
+// Close the path
+path.closePath()
+
+// Set attributes and stroke / fill
+UIColor.greenColor().setFill() // note this is a method in UIColor, not UIBezierPath
+UIColor.redColor().setStroke() // note this is a method in UIColor, not UIBezierPath
+path.lineWidth = 3.0             // method in UIBezierPath, not UIColor
+path.fill()                      // method in UIBezierPath
+path.stroke()                    // method in UIBezierPath
+
+var num = 0
+
+for index in 0...0 {
+    num += 1
+}
