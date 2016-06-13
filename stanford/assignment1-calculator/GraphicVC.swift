49585ea403f9eac8744330082a0257f61d0e7265
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-06-12 21:29:02 -0700

    added graphicVC and added split view

---
+++ stanford/assignment1-calculator/GraphicVC.swift
@@ -0,0 +1,18 @@
+//
+//  GraphicVC.swift
+//  assignment1-calculator
+//
+//  Created by jeffrey dinh on 6/12/16.
+//  Copyright © 2016 jeffrey dinh. All rights reserved.
+//
+
+import UIKit
+
+class GraphicVC: UIViewController {
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+    }
+ 
+
+}
