9b0e7b3ee88289f670bedb78113d614e742617d7
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-06-13 17:19:54 -0700

    added GraphVC

--- stanford/assignment1-calculator/GraphVC.swift
+++ stanford/assignment1-calculator/GraphVC.swift
@@ -1,18 +1,18 @@
 //
-//  GraphicVC.swift
+//  GraphVC.swift
 //  assignment1-calculator
 //
-//  Created by jeffrey dinh on 6/12/16.
+//  Created by jeffrey dinh on 6/13/16.
 //  Copyright © 2016 jeffrey dinh. All rights reserved.
 //
 
 import UIKit
 
-class GraphicVC: UIViewController {
-    
+class GraphVC: UIViewController {
+
+
+
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        
     }
- 
 
 }
