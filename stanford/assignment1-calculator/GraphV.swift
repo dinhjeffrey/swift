92e498afb8596bfd41c1c5123924700d9b32a289
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-06-19 21:45:19 -0700

    got graphing to work, just on a dummy line

--- stanford/assignment1-calculator/GraphV.swift
+++ stanford/assignment1-calculator/GraphV.swift
@@ -13,6 +13,7 @@ class GraphV: UIView {
     
     @IBInspectable var scale: CGFloat = 1.0 { didSet { setNeedsDisplay() }}
     @IBInspectable var graphOrigin: CGPoint? {didSet { setNeedsDisplay() }}
+    @IBInspectable var plotColor: UIColor = UIColor.redColor() {didSet { setNeedsDisplay() }}
     let pointsPerUnit: CGFloat = 50.0
     
     
@@ -22,13 +23,24 @@ class GraphV: UIView {
         }
         return graphOrigin!
     }
-    
+    var graphVC = GraphVC()
+    private func translatePlot(plot: (x: Double, y: Double)) -> CGPoint { // translates from axes api to human-readable numbers
+        let translatedX = CGFloat(plot.x) * pointsPerUnit * scale + graphCenter.x
+        let translatedY = CGFloat(-plot.y) * pointsPerUnit * scale + graphCenter.y
+        return CGPoint(x: translatedX, y: translatedY)
+    }
     
     override func drawRect(rect: CGRect) {
-        
         let axes = AxesDrawer(color: UIColor.blackColor(), contentScaleFactor: contentScaleFactor)
         axes.drawAxesInRect(bounds, origin: graphCenter, pointsPerUnit: pointsPerUnit * scale)
         let path = UIBezierPath()
+        if let plots = graphVC.graphPlot(self) {
+            path.moveToPoint(translatePlot((x: plots[0].x , y: plots[0].y))) // min bound x on screen, max bound x on screen. doesn't matter where you start as long as it is a point on the plot
+            for plot in plots {
+                path.addLineToPoint(translatePlot((x: plot.x, y: plot.y)))
+            }
+        }
+        plotColor.set()
         path.stroke()
     }
 }
--- stanford/assignment1-calculator/GraphVC.swift
+++ stanford/assignment1-calculator/GraphVC.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class GraphVC: UIViewController {
     
-    
     @IBOutlet weak var graphView: GraphV! {
         didSet { }
     }
@@ -18,27 +17,32 @@ class GraphVC: UIViewController {
         if recognizer.state == .Changed {
             let translation = recognizer.translationInView(graphView)
             graphView.graphOrigin = CGPoint(x: graphView.graphOrigin!.x + translation.x, y: graphView.graphOrigin!.y + translation.y)
-            
             recognizer.setTranslation(CGPointZero, inView: graphView) // By resetting the translation to zero all the time, you end up getting incremental translation
         }
-        
+    }
+    @IBAction func singleTap(recognizer: UITapGestureRecognizer) {
+        let pointTapped = recognizer.locationInView(graphView)
+        print("bounds.width are \(graphView.bounds.width)")
+        print("x: \(pointTapped.x) and y: \(pointTapped.y)")
     }
     
-
     @IBAction func doubleTapGraph(recognizer: UITapGestureRecognizer) {
-            let pointTapped = recognizer.locationInView(graphView)
-            print("x: \(pointTapped.x) and y: \(pointTapped.y)")
-            graphView.graphOrigin = CGPoint(x: pointTapped.x, y: pointTapped.y)
+        let pointTapped = recognizer.locationInView(graphView)
+        print("x: \(pointTapped.x) and y: \(pointTapped.y)")
+        graphView.graphOrigin = CGPoint(x: pointTapped.x, y: pointTapped.y)
     }
-    
     @IBAction func pinchGraph(recognizer: UIPinchGestureRecognizer) {
         if recognizer.state == .Changed {
             graphView.scale  *= recognizer.scale
             recognizer.scale = 1.0
         }
     }
-
-
-
-
+    var plots = [(x: Double, y: Double)]()
+    func graphPlot(sender: GraphV) -> [(x: Double, y: Double)]? {
+        for point in Int(0 - sender.graphCenter.x)...Int(sender.bounds.width - sender.graphCenter.x) {
+            plots.append((x: Double(point), y: 9))
+        }
+        //print(plots)
+        return plots
+    }
 }
